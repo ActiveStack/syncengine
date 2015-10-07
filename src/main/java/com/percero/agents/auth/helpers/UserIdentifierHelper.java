addfe9b852b2fa22a16c9814c96a0276246d6247
Jonathan Samples <jonnysamps@gmail.com>
2015-10-07 10:19:07 -0700

    More extracting hibernate

---
+++ src/main/java/com/percero/agents/auth/helpers/UserIdentifierHelper.java
@@ -0,0 +1,23 @@
+package com.percero.agents.auth.helpers;
+
+import com.percero.agents.auth.vo.IUserAnchor;
+import com.percero.agents.auth.vo.IUserIdentifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jonnysamps on 10/7/15.
+ */
+public class UserIdentifierHelper {
+    public static List<IUserIdentifier> getUserIdentifiersForUser(IUserAnchor user){
+        List<IUserIdentifier> result = new ArrayList<>();
+
+        return result;
+    }
+
+    public static IUserIdentifier getUserIdentifierForUserAndValue(String userId, String value){
+
+    }
+}
+
